346d043192a029eac7fabe2b084fe2a11b3459f4
irisgalgal <irisgalgal017@gmail.com>
2022-09-30 22:48:12 -0500

    Initial Commit

---
+++ wordChallenge/wordChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordChallengeApp.swift
+//  wordChallenge
+//
+//  Created by IrisDarka on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct wordChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
